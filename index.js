7665ad7d12097ce82d0a2c17d7cba77e511c1636
Josoe Santos Queiroz <jssantosqueiroz@gmail.com>
2020-06-06 09:42:26 -0300

    Add access log

--- index.js
+++ index.js
@@ -1,15 +1,18 @@
 const Discord = require('discord.js');
+require('dotenv').config()
 
 const client = new Discord.Client();
 
 let users = {};
 
 client.on('ready', () => {
-    console.log('Ready');
+    console.log('Ready and running!');
 });
 client.on('error', (e) => console.log(e));
 
-client.login(process.env.CLIENT_TOKEN);
+const token = process.env.CLIENT_TOKEN;
+
+client.login(token);
 
 function getUserFromMention(mention) {
 	if (!mention) return;
@@ -26,7 +29,8 @@ function getUserFromMention(mention) {
 }
 
 client.on('message' , message => {
-    
+    const now = new Date();
+    console.log(`[MESSAGE](${now.toISOString}):`);
     if(message.content.includes('\\re')) {
         const [command, args] = message.content.trim().split(' ');
         
@@ -42,8 +46,6 @@ client.on('message' , message => {
         message.channel.send(`<@${user.id}> ja reclamou ${users[user.id]} vezes.`);
 
         const n = Math.abs(~(Math.random() * 100));
-
-        console.log(n);
 
         if(Math.abs(n%3 === 0)) {
             message.channel.send(`Ei <@${user.id}> larga de ser chato`)
